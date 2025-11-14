e936c5ea0e1c6cb077d322948ca9a722efd96f14
github usernameDarlene250 <d.ayinkamiy@alustudent.com>
2025-11-14 11:47:59 -0800

    gather data

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
-"""
-Python script that returns information about an employee's
-TODO list progress using a REST API.
-"""
+
+"""Import libraries"""
 
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Fetch and display TODO list progress for a given employee.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/users/{employee_id}/todos"
-
-    # Fetch employee information and todos
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    if user_response.status_code != 200:
-        print("Error: Employee not found.")
-        return
-
-    employee = user_response.json()
-    todos = todos_response.json()
-
-    employee_name = employee.get("name")
-    total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed")]
-
-    print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task.get('title')}")
-
+"""Import libraries"""
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    user_id = int(sys.argv[1])
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Error: Employee ID must be an integer.")
-        sys.exit(1)
+    todo_data = requests.get(todos_url).json()
 
-    get_employee_todo_progress(employee_id)
+    employee_name = requests.get(users_url).json()["name"]
+
+    total_user_todos = 0
+    completed_todos = 0
+    titles = []
+    for todo in todo_data:
+        # Check if user_id is the same input as parameter
+        if user_id == todo["userId"]:
+            # Get the total number of todos
+            total_user_todos += 1
+            # Get the total number of completed tasks
+            if todo["completed"]:
+                completed_todos += 1
+                # Get the titles of the completed tasks
+                titles.append(todo["title"])
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_todos, total_user_todos))
+
+    for title in titles:
+        print("\t {}".format(title))
